878e31b874df741b825d2f6dd037179b707c8f40
mhardin <melinda.hardin@jocogov.org>
2020-04-01 08:14:11 -0500

    db user changed

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const connectDB = require('./config/db');
+
 const app = express();
+//connect to the database
+connectDB();
 
 app.get("/", (req, res) => res.send("API Runnig"));
 const PORT = process.env.PORT || 5000;
