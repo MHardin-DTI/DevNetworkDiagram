c41481ca11fc405a92a02956dd93fcd75d566551
mhardin <melinda.hardin@jocogov.org>
2020-04-01 15:05:33 -0500

    connected to MySql

---
+++ schema.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS network_diagram;
+
+CREATE DATABASE network_diagram;
+
+USE network_diagram;
+
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require("express");
-const connectDB = require("./config/db");
+const connectDB = require("./config/connection");
 
 const app = express();
 //connect to the database
-connectDB();
-
-//connecting to database
-connectDB();
+connectDB;
 
 app.get("/", (req, res) => res.send("API Runnig"));
 
 app.use("/api/apps", require("./routes/api/apps"));
 app.use("/api/servers", require("./routes/api/servers"));
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
